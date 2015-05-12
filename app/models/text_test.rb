d020c82ec125f6a7f68c3c8f8f3d26499e00c5b2
Ryan Matuszewski <rmmatuszewski@gmail.com>
2015-05-12 15:43:30 -0400

    api render

--- app/models/text_test.rb
+++ app/models/text_test.rb
@@ -1,7 +1,33 @@
 require './alchemyapi'
 alchemyapi = AlchemyAPI.new()
 
-myText = "i can't tell whether i like or dislike my job!"
+# myText = "i can't tell whether i like or dislike!"
+myUrl = "http://www.nytimes.com/2015/05/13/business/dealbook/verizon-to-buy-aol-for-4-4-billion.html?hp&action=click&pgtype=Homepage&module=first-column-region&region=top-news&WT.nav=top-news&_r=0"
 
-response = alchemyapi.sentiment("text", myText)
-puts "Sentiment: " + response["docSentiment"]["type"]
+response = alchemyapi.entities("url", myUrl, {'sentiment'=>1})
+# puts "type: " + response["docSentiment"]["type"]
+
+if response['status'] == 'OK'
+	puts '## Response Object ##'
+	puts JSON.pretty_generate(response)
+
+
+	puts ''
+	puts '## Entities ##'
+	for entity in response['entities']
+		puts 'text: ' + entity['text']
+		puts 'type: ' + entity['type']
+		puts 'relevance: ' + entity['relevance']
+		print 'sentiment: ' + entity['sentiment']['type'] 
+		
+		#Make sure score exists (it's not returned for neutral sentiment
+		if entity['sentiment'].key?('score')
+			print ' (' + entity['sentiment']['score'] + ')'
+		end
+
+		puts ''
+	end
+else
+	puts 'Error in entity extraction call: ' + response['statusInfo']
+end
+
--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,9 @@ module WebPortalApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    
     config.active_record.raise_in_transactional_callbacks = true
+
+    # config.autoload_paths += %W(#{config.root}/vendor)
   end
 end
